5913408c13716f8f8b3c95cc846c3b996491a953
geekrope <geekrope@gmail.com>
2024-03-29 19:22:43 +0400

    Segment tree data structure

--- homework_18/Program.cs
+++ homework_18/Program.cs
@@ -3,6 +3,156 @@ using System.Collections.Generic;
 
 namespace homework_18
 {
+    interface ISegmentTreeNode
+    {
+        int Left
+        {
+            get;
+        }
+        int Right
+        {
+            get;
+        }
+        int Data
+        {
+            get;
+        }
+
+        void Update(int index, int value);
+        int? Query(int left, int right);
+    }
+    public class SegmentTreeNode : ISegmentTreeNode
+    {
+        private int left, right, data;
+        private Func<int, int, int> comparator;
+        private ISegmentTreeNode leftChild, rightChild;
+
+        public int Left => left;
+        public int Right => right;
+        public int Data => data;
+
+        public void Update(int index, int value)
+        {
+            if (left <= index && right >= index)
+            {
+                leftChild.Update(index, value);
+                rightChild.Update(index, value);
+
+                data = comparator(leftChild.Data, rightChild.Data);
+            }
+            else
+            {
+                return;
+            }
+        }
+        public int? Query(int left, int right)
+        {
+            if (this.right < left || this.left > right)
+            {
+                return null;
+            }
+            else if (this.left >= left && this.right <= right)
+            {
+                return data;
+            }
+            else
+            {
+                var query1 = leftChild.Query(left, right);
+                var query2 = rightChild.Query(left, right);
+
+                if (query1.HasValue && !query2.HasValue)
+                {
+                    return query1.Value;
+                }
+                else if (!query1.HasValue && query2.HasValue)
+                {
+                    return query2.Value;
+                }
+                else if (query1.HasValue && query2.HasValue)
+                {
+                    return comparator(query1.Value, query2.Value);
+                }
+
+                return null;
+            }
+        }
+
+        public SegmentTreeNode(int left, int right, int[] array, Func<int, int, int> comparator)
+        {
+            this.left = left;
+            this.right = right;
+            this.comparator = comparator;
+
+            if (right > left)
+            {
+                var mid = (left + right) / 2;
+                leftChild = mid == left ? (ISegmentTreeNode)(new SegmentTreeLeaf(mid, array)) : (ISegmentTreeNode)(new SegmentTreeNode(left, mid, array, comparator));
+                rightChild = right == mid + 1 ? (ISegmentTreeNode)(new SegmentTreeLeaf(right, array)) : (ISegmentTreeNode)(new SegmentTreeNode(mid + 1, right, array, comparator));
+                data = comparator(leftChild.Data, rightChild.Data);
+            }
+            else
+            {
+                throw new ArgumentException();
+            }
+        }
+    }
+    public class SegmentTreeLeaf : ISegmentTreeNode
+    {
+        private int index, data;
+
+        public int Left => index;
+        public int Right => index;
+        public int Data => data;
+
+        public void Update(int index, int value)
+        {
+            if (this.index == index)
+            {
+                data = value;
+            }
+        }
+        public int? Query(int left, int right)
+        {
+            if (left <= index && right >= index)
+            {
+                return data;
+            }
+
+            return null;
+        }
+
+        public SegmentTreeLeaf(int index, int[] array)
+        {
+            this.index = index;
+            this.data = array[index];
+        }
+    }
+    public class SegmentTree
+    {
+        private ISegmentTreeNode root;
+
+        public void Update(int index, int value)
+        {
+            root.Update(index, value);
+        }
+        public int? Query(int left, int right)
+        {
+            return root.Query(left, right);
+        }
+
+        public SegmentTree(int[] array, Func<int, int, int> comparator)
+        {
+            if (array.Length == 1)
+            {
+                root = new SegmentTreeLeaf(0, array);
+            }
+            else
+            {
+                root = new SegmentTreeNode(0, array.Length - 1, array, comparator);
+            }
+        }
+    }
+
     public class Program
     {
         public static int[] FindMinMax(int[] array, int sampleLength, Func<int, int, int> comparator, int defaultValue)
@@ -22,7 +172,7 @@ namespace homework_18
                 max = comparator(max, array[i]);
             }
 
-            if (array.Length % sampleLength != 0)
+            if (maxes.Count * sampleLength < array.Length)
             {
                 maxes.Add(max);
             }
@@ -33,31 +183,36 @@ namespace homework_18
         {
             var max = defaultValue;
 
-            if (((start + sampleLength - 1) / sampleLength) * sampleLength > array.Length || end - start < sampleLength)
+            while (start % sampleLength != 0 && start <= end)
             {
-                for (var i = start; i <= end; i++)
-                {
-                    max = comparator(max, array[i]);
-                }
+                max = comparator(max, array[start]);
+                start++;
             }
-            else
+
+            while (end % sampleLength != 0 && start <= end)
             {
-                while (start % sampleLength != 0)
-                {
-                    max = comparator(max, array[start]);
-                    start++;
-                }
+                max = comparator(max, array[end]);
+                end--;
+            }
 
-                while (end % sampleLength != 0)
-                {
-                    max = comparator(max, array[end]);
-                    end--;
-                }
-
+            if (start < end)
+            {
                 for (int i = start / sampleLength; i < end / sampleLength; i++)
                 {
                     max = comparator(max, maxes[i]);
                 }
+            }
+
+            return max;
+        }
+
+        public static int MaxMinOnSegemnt(int[] array, int start, int end, Func<int, int, int> comparator, int defaultValue)
+        {
+            var max = defaultValue;
+
+            for (var i = start; i <= end; i++)
+            {
+                max = comparator(max, array[i]);
             }
 
             return max;
@@ -125,7 +280,7 @@ namespace homework_18
                 Console.WriteLine(o);
             }
         }
-        public static void B()
+        public static void B() //Memory limit
         {
             var n = int.Parse(Console.ReadLine());
             var input = Console.ReadLine().Split(new string[] { " " }, StringSplitOptions.RemoveEmptyEntries);
@@ -155,7 +310,38 @@ namespace homework_18
                 Console.Write(output + " ");
             }
         }
-        public static void Main(string[] args)
+        public static void C() //Wrong answer
+        {
+            var n = Console.ReadLine();
+            var input = Console.ReadLine().Split(new string[] { " " }, StringSplitOptions.RemoveEmptyEntries);
+            var m = int.Parse(Console.ReadLine());
+
+            var array = new int[input.Length];
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                array[i] = int.Parse(input[i]);
+            }
+
+            var tree = new SegmentTree(array, Math.Max);
+
+            //var sampleLength = (int)Math.Sqrt(array.Length);
+            //var maxes = FindMinMax(array, sampleLength, Math.Max, 0);
+            //FindMinMax(array, maxes, sampleLength, s, e, Math.Max, 0)
+            var output = " ";
+
+            for (int i = 0; i < m; i++)
+            {
+                var query = Console.ReadLine().Split(new string[] { " " }, StringSplitOptions.RemoveEmptyEntries);
+                var s = int.Parse(query[0]) - 1;
+                var e = int.Parse(query[1]) - 1;
+
+                output += tree.Query(s, e) + " ";
+            }
+
+            Console.WriteLine(output);
+        }
+        public static void D() //Wrong answer
         {
             var n = Console.ReadLine();
             var input = Console.ReadLine().Split(new string[] { " " }, StringSplitOptions.RemoveEmptyEntries);
@@ -176,12 +362,17 @@ namespace homework_18
             {
                 var query = Console.ReadLine().Split(new string[] { " " }, StringSplitOptions.RemoveEmptyEntries);
                 var s = int.Parse(query[0]) - 1;
-                var f = int.Parse(query[1]) - 1;
+                var e = int.Parse(query[1]) - 1;
 
-                output += FindMinMax(array, maxes, sampleLength, s, f, Math.Min, int.MaxValue) + " ";
+                output += FindMinMax(array, maxes, sampleLength, s, e, Math.Min, int.MaxValue) + " ";
             }
 
             Console.WriteLine(output);
+        }
+
+        public static void Main(string[] args)
+        {
+            C();
         }
     }
 }
